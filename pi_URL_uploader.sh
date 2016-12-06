faf79d459bb2c23aac820df97d12dfbed7a7de19
oredla <oredla@gmail.com>
2016-12-05 21:51:16 -0500

    first upload for pi_URL_uploader.sh
    initial upload

---
+++ pi_URL_uploader.sh
@@ -0,0 +1,7 @@
+# Send to IFTTT
+secret_key="SECRET_KEY"
+value1=`hostname -f | cut -d" " -f1`
+value2=`hostname -I | cut -d" " -f1`
+value3=`hostname -I | cut -d" " -f1`
+json="{\"value1\":\"${value1}\",\"value2\":\"${value2}\",\"value3\":\"${value3}\"}"
+curl -X POST -H "Content-Type: application/json" -d "${json}" https://maker.ifttt.com/trigger/pi_URL_uploader/with/key/${secret_key}  
