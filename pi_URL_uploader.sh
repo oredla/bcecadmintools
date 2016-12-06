59b64ccee01e99267c98f4f99a7436199904f808
oredla <oredla@gmail.com>
2016-12-05 22:35:35 -0500

    update to 2 values only

--- pi_URL_uploader.sh
+++ pi_URL_uploader.sh
@@ -2,6 +2,5 @@
 secret_key="SECRET_KEY"
 value1=`hostname -f | cut -d" " -f1`
 value2=`hostname -I | cut -d" " -f1`
-value3=`hostname -I | cut -d" " -f1`
-json="{\"value1\":\"${value1}\",\"value2\":\"${value2}\",\"value3\":\"${value3}\"}"
+json="{\"value1\":\"${value1}\",\"value2\":\"${value2}\"}"
 curl -X POST -H "Content-Type: application/json" -d "${json}" https://maker.ifttt.com/trigger/pi_URL_uploader/with/key/${secret_key}  
